6d812678cd3f857f54db8dd5cce50b6eb4d5f4f1
Oleksandr Sachuk <oleksandr29sachuk@gmail.com>
2022-02-02 12:08:37 +0200

    1 is done

---
+++ pz_1.9/pz_1.9_01.js
@@ -0,0 +1,17 @@
+let a = 0;
+
+while (a<=100){
+    let flag = 0;
+    let b = 2;
+    while(b<a) {
+        if (a % b == 0) {
+            flag = 1;
+            break;
+        }
+        b++;
+    }
+    if(a>1 && flag==0){
+        console.log(a);
+    }
+    a++;
+}
