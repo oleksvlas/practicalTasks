8b8c19777faa93ef3af82501cbd0beb5e343c1b0
Andy Mychka <I.Muchka73@gmail.com>
2022-02-02 12:30:15 +0200

    first

---
+++ pz_1.9/pz_1.9_01.js
@@ -0,0 +1,47 @@
+console.log("Ответ на первое задание");
+        
+//      Создаем массив от 1 до 100
+        var arr = [];
+        var end = 1;
+        while (end < 100){
+            end++;
+            arr.push(end);
+        }
+//      Удаляем все четные цифры
+        var a = 2;
+        var b = 2;
+        while (a <= 100){
+            a += b;
+                arr.splice(arr.indexOf(a), 1);
+        }
+//      Удаляет все цифры которые деляться на нечетные до 10
+        a = 4;
+        b = 3;
+        while (a < 100){
+            if (a % b ===0 && arr.indexOf(a) !== -1){
+                arr.splice(arr.indexOf(a), 1);
+            }else {
+                a++;
+            }
+        }
+        a = 6;
+        b = 5;
+        while (a < 100) {
+            if (a % b === 0 && arr.indexOf(a) !== -1) {
+                arr.splice(arr.indexOf(a), 1);
+            } else {
+                a++;
+            }
+        }
+
+        a = 8;
+        b = 7;
+        while (a < 100) {
+            if (a % b === 0 && arr.indexOf(a) !== -1) {
+                arr.splice(arr.indexOf(a), 1);
+            } else {
+                a++;
+            }
+        }
+
+        console.log(arr);
