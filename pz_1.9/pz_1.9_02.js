43e67a6d9af9428b406d2074fe70250d0f4e0efe
Oleksandr Sachuk <oleksandr29sachuk@gmail.com>
2022-02-02 12:22:20 +0200

    2 is done

---
+++ pz_1.9/pz_1.9_02.js
@@ -0,0 +1,13 @@
+let start = 0;
+
+do{
+    let description = "";
+    if (start == 0)
+        description = " - це нуль";
+    if (start % 2 == 0)
+        description = " - це парне";
+    else
+        description = " - це непарне"
+    console.log(start+description);
+    start++;
+} while (start<=20)
