0c2f648bbd920f3bfec36cca803154091380ebf2
olgaturchak <olgaturchak14@gmail.com>
2023-02-03 17:09:02 +0200

    corrections task4

--- pz_1.11/task4.js
+++ pz_1.11/task4.js
@@ -44,6 +44,25 @@ function check(...values) {
     }
     return true
 }
+function calc(num1, num2, func){
+   return func(num1, num2)
+}
 
-console.log(sub(1, "324"))
-console.log(div(10, 0))
+let num1 = parseInt(prompt("Enter num1: "));
+let num2 = parseInt(prompt("Enter num2: "));
+let operation = prompt("Enter an operation: 1-Add  2-Sub  3-Mul  4-Div");
+switch (parseInt(operation)){
+    case 1:
+        console.log("Calc(" + num1 + "," + num2 + ", Add) ==> " + calc(num1, num2, add))
+        break
+    case 2:
+        console.log("Calc(" + num1 + "," + num2 + ", Sub) ==> " + calc(num1, num2, sub))
+        break
+    case 3:
+        console.log("Calc(" + num1 + "," + num2 + ", Mul) ==> " + calc(num1, num2, mul))
+        break
+    case 4:
+        console.log("Calc(" + num1 + "," + num2 + ", Div) ==> " + calc(num1, num2, div))
+        break
+}
+
