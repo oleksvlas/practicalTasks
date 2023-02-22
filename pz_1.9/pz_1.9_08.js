52806ba0a7442ab7976d89edbe7bea1090569e8a
Roman Bondar <romanbondar72@gmail.com>
2023-02-22 18:05:22 +0200

    pz_1.9_08.js

---
+++ pz_1.9/pz_1.9_08.js
@@ -0,0 +1,28 @@
+let c = prompt("Введіть число від 1 до 7");
+
+switch(c){
+    case "1":
+        alert("Понеділок")
+        break;
+    case "2":
+        alert("Вівторок")
+        break;
+    case "3":
+        alert("Середа")
+        break;
+    case "4":
+        alert("Четвер")
+        break;
+    case "5":
+        alert("Пʼятниця")
+        break;
+    case "6":
+        alert("Субота")
+        break;
+    case "7":
+        alert("Неділя")
+        break;
+    default:
+        alert("помилка!!!")
+        break;
+}
