ff51d3ed7d5bef93231bdadd4866968f3e7bc414
Roman Bondar <romanbondar72@gmail.com>
2023-02-24 16:32:45 +0200

    pz_1.11_02.js

--- pz_1.11/pz_1.11_01.js
+++ pz_1.11/pz_1.11_01.js
@@ -4,7 +4,7 @@ function new_function(n){
     let arr = [];
     for(let i = n; i > 0; i--){
         arr.push(i);}
-        console.log("n =", n, "==>", arr);
+    console.log("n =", n, "==>", arr);
 }
 
 new_function(n);
