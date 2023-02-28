5046a61c8f0de7ff745c17c8501c79481dbead8b
vladlenmarchenko <markelovwtf7@gmail.com>
2023-02-28 11:45:43 +0200

    1.11_5  ready

--- pz_1.11/pz_1.11_5.js
+++ pz_1.11/pz_1.11_5.js
@@ -2,8 +2,38 @@ const check = () => {
     let numeric = Number(prompt("Enter some numeric"))
 
     console.log(numeric > 0 ? "Is positive" : "Isn`t positive");
-    console.log(numeric ? "Is positive" : "Isn`t positive");
+    if (numeric % 2 !== 0 &&
+        numeric % 3 !== 0 &&
+        numeric % Math.sqrt(numeric) !== 0 &&
+        numeric % 5 !== 0 &&
+        numeric % 7 !== 0 ||
+        numeric === 2 ||
+        numeric === 3 ||
+        numeric === 5 ||
+        numeric === 7
+    ) {
+        console.log("Numeric is prime")
+    } else {
+        console.log("Numeric isn`t prime")
+    }
+
+    if (numeric % 2 === 0) {
+        console.log("Numeric is divide on 2")
+    }
+    if (numeric % 5 === 0) {
+        console.log("Numeric is divide on 5")
+    }
+    if (numeric % 3 === 0) {
+        console.log("Numeric is divide on 3")
+    }
+    if (numeric % 6 === 0) {
+        console.log("Numeric is divide on 6")
+    }
+    if (numeric % 9 === 0) {
+        console.log("Numeric is divide on 9")
+    }
 
 }
+
 
 check()
