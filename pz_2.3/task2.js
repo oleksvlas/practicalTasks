02b83c620754a9e096f6cca6f1379560c1dca15e
Eugene Kornienko <zhekag656@gmail.com>
2023-03-30 21:53:29 +0300

    pz_2.3 last upd

--- pz_2.3/task2.js
+++ pz_2.3/task2.js
@@ -5,6 +5,6 @@ const player = require("play-sound")();
 console.log(chalk.red("Hello world"));
 console.log(chalk.blue("Eugene ") + chalk.yellow("Kornienko"));
 
-player.play(path.join(__dirname, "pedik.mp3"), {timeout: 300}, function(err){
+player.play(path.join(__dirname, "song.mp3"), {timeout: 300}, function(err){
     if (err) throw err
 });
